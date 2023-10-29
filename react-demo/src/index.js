389cfe68ce8e004d7d79fd5023483cb9c1066c96
CodeSandbox <support@codesandbox.io>
2023-10-29 09:54:40 +0000

    Sandbox snapshot commit

---
+++ react-demo/src/index.js
@@ -0,0 +1,5 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import AppComponent from "./app"
+const root=ReactDOM.createRoot(document.getElementById('root'))
+root.render(<AppComponent fname="john" role="admin"></AppComponent>)
