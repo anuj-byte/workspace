78d580e2213ca0cd68bc9c2baf592b720c78aec7
anuj-byte” <sanuj3781@gmail.com>
2023-11-04 16:17:04 +0000

    Sandbox snapshot commit

--- react-demo-Cra/main-react/src/components/Todo.js
+++ react-demo-Cra/main-react/src/components/Todo.js
@@ -5,21 +5,41 @@ class TodoComponent extends React.Component{ //class react component is defined 
         super(props) //super keyword is use in class component to use parents props
         this.state={ //state is use to re render the react dom and show our changes value in our UI in real time but in variable storage we could not see the value on UI after updating
             data:[
-                {
-                    title:this.props.data[0].title,
-                    text:this.props.data[0].text
-                }
-            ]
+                
+            ],
+            currenttitle:"",
+            currenttext:"",
+            id:0
         }
         
        
     }
     addItem=()=>{
-        console.log("function is called")
+        this.setState({id:this.state.id+1})
         this.setState({data:[...this.state.data,{//The JavaScript spread operator (...) allows us to quickly copy all or part of an existing array or object into another array or object.
-            title:"css",
-            text:"solve css Problem"
+            id:this.state.id,
+            title:this.state.currenttitle,
+            text:this.state.currenttext,
+            completed:false //for changing the completion status when needed
         }]})
+    }
+    HandleText=(ev)=>{ //these is function to add the value of text input here
+        console.log(ev.target.value)
+        this.setState({currenttext:ev.target.value})
+
+    }
+    HandleTitle=(ev)=>{ //these is the function is to add the value of title input here
+        console.log(ev.target.value)
+        this.setState({currenttitle:ev.target.value})
+
+    }
+    CompletionStatus=(itemid)=>{
+      const temp=[...this.state.data]
+      //const obj =temp.find((iteam)=>iteam.id===itemid)
+      //obj.completed=!obj.completed
+      console.log("hi i am here")
+      
+      
     }
 
 
@@ -27,13 +47,19 @@ render(){ //render is used in class component to render the jsx
     console.log(this.state.data)
     return(
         <div>
-            <button onClick={this.addItem}>Add-Item</button>
+            Title:<input type="text" onChange={(e)=>this.HandleTitle(e)}></input>
+            Text:<input type="text" onChange={(e)=>this.HandleText(e)}></input>
+           <button onClick={this.addItem}>Add-Item</button>
         {
         this.state.data.map((iteam)=>(
-            <div>
+            iteam.completed?<></>:
+             <div>
+            <h1>{iteam.id}</h1>
             <h1>{iteam.title}</h1>
             <p>{iteam.text}</p>
+            <button onClick={()=>this.CompletionStatus(iteam.id)} >done</button>
             </div>
+           
 
     ))
         }
