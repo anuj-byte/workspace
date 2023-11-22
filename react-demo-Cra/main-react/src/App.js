45062eda96914180e2a2f8495b42831ec5886123
anuj-byte” <sanuj3781@gmail.com>
2023-11-22 10:14:59 +0000

    new commit

--- react-demo-Cra/main-react/src/App.js
+++ react-demo-Cra/main-react/src/App.js
@@ -7,10 +7,10 @@ import HomePage from "./components/SPA_Components/ViewMentor";
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        {/*<Link to="/signup">Signup</Link>
+      <BrowserRouter> {/*browser route is used to wrap the routes*/} 
+        {/*<Link to="/signup">Signup</Link>Link is used as a hyperlink and change that perticular components in same page 
         <Link to="/Dashboard">Dashboard</Link>*/}
-        <Routes>
+        <Routes>{/*Routes is use to set a group of route in it which is further use to define the path to given components*/} 
           <Route path="/" element={<HomePage></HomePage>}></Route>
           <Route
             path="/signup"
